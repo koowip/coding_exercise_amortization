1d897800d106001a77d8eba1b98bbcb713107c78
Patrick White <patrickwhite.swe@gmail.com>
2023-12-05 16:35:37 -0700

    Files added

---
+++ src/Components/DataTableComp.tsx
@@ -0,0 +1,32 @@
+
+export default function DataTableComp({amoritizationData}) {
+
+  return(
+    <table>
+    <thead>
+      <tr>
+        <th>Month of Loan</th>
+        <th>Balance</th>
+        <th>Principal Paid</th>
+        <th>Interest</th>
+        <th>Payment</th>
+        <th>Total Interest</th>
+        <th>Total Paid</th>
+      </tr>
+    </thead>
+    <tbody>
+      {amoritizationData.map((data: any, index: number) => (
+        <tr key={index}>
+          <td>{data.monthOfLoan}</td>
+          <td>{data.balance}</td>
+          <td>{data.principalPaid}</td>
+          <td>{data.interestPaid}</td>
+          <td>{data.payment}</td>
+          <td>{data.totalInterest}</td>
+          <td>{data.totalPaid}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+  );
+};
